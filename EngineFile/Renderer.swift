11044980f77b0f6b455d5bc052a2a82d782ee247
Naiwen <sws@swsdeMac-mini.local>
2021-06-03 20:25:14 +0800

    add renderer

---
+++ EngineFile/Renderer.swift
@@ -0,0 +1,8 @@
+//
+//  Renderer.swift
+//  MetalView
+//
+//  Created by sws on 2021/6/3.
+//
+
+import Foundation
---
+++ EngineFile/Scene/Triangle.swift
@@ -0,0 +1,8 @@
+//
+//  Triangle.swift
+//  MetalView
+//
+//  Created by sws on 2021/6/3.
+//
+
+import Foundation
