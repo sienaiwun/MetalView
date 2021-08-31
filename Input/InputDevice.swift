5f0a21786aa3f7d1675a823a00a00d506ac30c22
Naiwen <sws@swsdeMac-mini.local>
2021-08-31 15:28:47 +0800

    mac ios input

--- Input/InputDevice.swift
+++ Input/InputDevice.swift
@@ -1,7 +1,20 @@
 import MetalKit
 
+class Touch
+{
+    var pos:FLOAT2
+    var startPos:FLOAT2
+    var delta:FLOAT2
+    init(pos:FLOAT2,startPos:FLOAT2,delta:FLOAT2) {
+        self.pos = pos
+        self.startPos = startPos
+        self.delta = delta
+    }
+};
 
-class Input
+
+
+class InputDevice
 {
     public var name: String = "Basic Vertex Shader"
     public var mouseDeltaX : Float = 0.0
@@ -15,11 +28,15 @@ class Input
     public func update() {
         mouseDeltaX = 0.0
         mouseDeltaY = 0.0
-        for var touch in touches
+        for touch in touches
         {
             mouseDeltaX = touch.delta.x;
             mouseDeltaY = touch.delta.y;
         }
+    }
+    
+    public func clear(){
+        touches.removeAll()
     }
     
     
---
+++ Input/Touch.swift
@@ -0,0 +1,8 @@
+//
+//  Touch.swift
+//  MetalView
+//
+//  Created by sws on 2021/8/30.
+//
+
+import Foundation
