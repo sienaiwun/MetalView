a4dc545b4676290286e6a65087edfe45d9faab27
Naiwen <sws@swsdeMac-mini.local>
2021-09-10 13:43:36 +0800

    ios msaa control

--- IOS/LauchViewController.swift
+++ IOS/LauchViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class LauchViewController: UIViewController {
 
+    @IBAction func onChanged(_ sender: UISegmentedControl!) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            Engine.msaaSample = 1
+        case 1:
+            Engine.msaaSample = 2
+        case 2:
+            Engine.msaaSample = 4
+        default:
+            Engine.msaaSample = 1
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
